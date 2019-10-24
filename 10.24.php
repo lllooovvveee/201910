28dfe5338101083f28a65e1252f20882a40b28fc
lllooovvveee <56526445+lllooovvveee@users.noreply.github.com>
2019-10-24 15:42:45 +0800

    Create 10.24.php

---
+++ 10.24.php
@@ -0,0 +1,5 @@
+<form action="http://wwww.baidu.com/baidu" target="_blank">
+                        <input id="km" type="text" baiduSug="1" size="30" name="word">
+                        <input type="submit" value="百度">
+                    </form>
+                    <script charset="gbk" src="http://www.baidu.com/js/opensug.js"></script>
